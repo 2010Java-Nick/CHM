b2b22ea051b1ea579547d749f5adacb76ba8c4dd
unknown <leonardoz@allegheny.edu>
2020-12-12 17:53:55 -0500

    Added spring jdbc, h2 db to gradle build

--- src/test/java/CHM/test/dao/ProfileDaoHibernate.java
+++ src/test/java/CHM/test/dao/ProfileDaoHibernate.java
@@ -9,6 +9,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -38,8 +39,14 @@ public class ProfileDaoHibernate {
 	@After
 	public void tearDown() throws Exception {
 	}
+	
+	@Test
+	public void testSelectProfile() {
+		fail("Not yet implemented");
+	}
 
 	@Test
+	@Rollback(true)
 	public void testInsertProfile() {
 		fail("Not yet implemented");
 	}
