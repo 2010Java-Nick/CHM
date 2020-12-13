31c800dd29ad8bfe50651f7a51d686122dcb58fd
unknown <leonardoz@allegheny.edu>
2020-12-12 19:03:46 -0500

    final commit for tonight

--- src/main/java/CHM/config/TestConfig.java
+++ src/main/java/CHM/config/TestConfig.java
@@ -21,7 +21,7 @@ public class TestConfig {
 	
 	//not sure if I need these, not sure how to implement
 	private static final String DATABASE_DRIVER = "org.h2.Driver";
-	private static final String DATABASE_URL = "jdbc:h2:mem:test";
+	private static final String DATABASE_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
 	private static final String DATABASE_USERNAME = "sa";
 	private static final String DATABASE_PASSWORD = "";
 	private static final String HIBERNATE_DIALECT = "org.hibernate.dialect.H2Dialect";
