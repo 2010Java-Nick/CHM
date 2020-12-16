5210517dde1984c0170fde4b9b48709d1e8ffbb8
unknown <leonardoz@allegheny.edu>
2020-12-15 21:11:15 -0500

    Added profile creation service impl

--- src/main/java/CHM/service/ProfileServiceImpl.java
+++ src/main/java/CHM/service/ProfileServiceImpl.java
@@ -2,14 +2,36 @@ package CHM.service;
 
 import java.util.List;
 
+import org.hibernate.HibernateException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import CHM.dao.ProfileDaoHibernate;
 import CHM.model.Profile;
 
+@Service
 public class ProfileServiceImpl implements ProfileService {
+	
+	ProfileDaoHibernate profileDao;
+
+	/**
+	 * @param profileDao the profileDao to set
+	 */
+	@Autowired
+	public void setProfileDao(ProfileDaoHibernate profileDao) {
+		this.profileDao = profileDao;
+	}
 
 	@Override
 	public Profile createProfile(Profile profile) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			profileDao.insertProfile(profile);
+			return profile;
+			
+		} catch (HibernateException e) {
+			return null;
+		}
+		
 	}
 
 	@Override
