b3f9c142e14def44da133c8bf4328c752d0bdac6
Zachary Leonardo <zachleonardo15@gmail.com>
2020-12-17 18:55:55 -0500

    added service impl tests

--- src/main/java/CHM/dao/InterestDaoHibernate.java
+++ src/main/java/CHM/dao/InterestDaoHibernate.java
@@ -71,23 +71,25 @@ public class InterestDaoHibernate implements InterestDao {
 	}
 
 	@Override
-	public void updateInterest(Interest interest) throws HibernateException {
+	public Interest updateInterest(Interest interest) throws HibernateException {
 		
 		Session sess = sessionFactory.openSession();
 		Transaction tx = sess.beginTransaction();
 		sess.update(interest);
 		tx.commit();
 		sess.close();
+		return interest;
 	}
 
 	@Override
-	public void deleteInterest(Interest interest) throws HibernateException {
+	public boolean deleteInterest(Interest interest) throws HibernateException {
 		
 		Session sess = sessionFactory.openSession();
 		Transaction tx = sess.beginTransaction();
 		sess.delete(interest);
 		tx.commit();
 		sess.close();
+		return true;
 	}
 
 
