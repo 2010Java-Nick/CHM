5f5525f537081506f68ce648b2a599bc35888c70
Zachary Leonardo <zachleonardo15@gmail.com>
2020-12-18 18:59:55 -0500

    finished refactor of photo controller, added commons-fileupload dependency and multipart file configuration

--- src/main/java/CHM/controller/PhotoController.java
+++ src/main/java/CHM/controller/PhotoController.java
@@ -1,5 +1,6 @@
 package CHM.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,16 +8,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import CHM.model.Photo;
 import CHM.service.PhotoServiceImpl;
 
-@RestController(value = "/photo")
+@RestController
 public class PhotoController {
 	
 	PhotoServiceImpl photoService;
@@ -29,7 +31,7 @@ public class PhotoController {
 		this.photoService = photoService;
 	}
 	
-	@PostMapping
+	@PostMapping(path = "/photo")
 	public ResponseEntity<Integer> createPhoto(@RequestBody Photo photo) {
 		
 		Integer photoId = photoService.createPhoto(photo);
@@ -40,7 +42,7 @@ public class PhotoController {
 		return re;
 	}
 	
-	@GetMapping(path = "/{photoId}")
+	@GetMapping(path = "/photo/{photoId}")
 	public ResponseEntity<Photo> readPhotoById(@PathVariable("photoId")int photoId) {
 		
 		Photo photo = photoService.readPhotoById(photoId);
@@ -48,7 +50,7 @@ public class PhotoController {
 		return re;
 	}
 	
-	@GetMapping(path = "/profile/{profileId}")
+	@GetMapping(path = "/photo/profile/{profileId}")
 	public ResponseEntity<List<Photo>> readPhotosByProfile(@PathVariable("profileId")int profileId) {
 		
 		List<Photo> photoList = photoService.readPhotosByProfileId(profileId);
@@ -56,18 +58,27 @@ public class PhotoController {
 		return re;
 	}
 	
-	@PatchMapping
-	public ResponseEntity<Photo> updatePhoto(@RequestBody Photo photo) {
+	@PostMapping(path = "/photo/{photoId}")
+	public ResponseEntity<Photo> updatePhoto(@PathVariable("photoId")int photoId, @RequestParam("photo") MultipartFile photo) {
 		
-		Photo updatedPhoto = photoService.updatePhoto(photo);
-		ResponseEntity<Photo> re = new ResponseEntity<Photo>(updatedPhoto, HttpStatus.BAD_REQUEST);
-		if (updatedPhoto != null) {
-			re = new ResponseEntity<Photo>(updatedPhoto, HttpStatus.OK);
-		}
+		Photo toUpdate = photoService.readPhotoById(photoId);
+		ResponseEntity<Photo> re = new ResponseEntity<Photo>(toUpdate, HttpStatus.BAD_REQUEST); 
+		
+		try {
+			if(!photo.isEmpty()) {
+				
+				byte[] bytes = photo.getBytes();
+				toUpdate.setPhoto(bytes);
+				Photo updatedPhoto = photoService.updatePhoto(toUpdate);
+				re = new ResponseEntity<Photo>(updatedPhoto, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			return re;
+		}	
 		return re;
 	}
 	
-	@DeleteMapping
+	@DeleteMapping(path = "/photo")
 	public ResponseEntity<Boolean> deletePhoto(@RequestBody Photo photo){
 		
 		Boolean deleted = photoService.deletePhoto(photo);
