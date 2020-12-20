1fa678c3486438a3099d038f4afc2625baa93077
Chris Breniser <chrisbreniser@gmail.com>
2020-12-19 21:26:44 -0800

    Messing with checkbox display and have not finished

--- CHM-frontend/CHM/src/app/components/interests/interests.component.ts
+++ CHM-frontend/CHM/src/app/components/interests/interests.component.ts
@@ -10,28 +10,56 @@ import { InterestService } from 'src/app/services/interest.service';
 })
 export class InterestsComponent implements OnInit {
 
-  interests = ['Dogs','Cats','Snakes','Pigs','Horses','Whales'];
-  limit: number = 3;
-  checked: number = 0;
+  // interests = ['Dogs', 'Cats', 'Snakes', 'Pigs', 'Horses', 'Whales'];
+  interests = [
+    {value:false, lable: 'Dogs'},
+    {value:false, lable: 'Cats'},
+    {value:false, lable: 'Snakes'},
+    {value:false, lable: 'Pigs'},
+    {value:false, lable: 'Horses'},
+    {value:false, lable: 'Whales'}
+  ];
+  limit = 3;
+  checked = 0;
 
   ngOnInit(): void {
 
   }
 
-  
-  checkChanged(interest: any) {
-    if (interest.checked){ 
+
+  // checkChanged(interest: any) {
+  //   if (interest.target.checked){ 
+  //     this.checked++;
+  //   }
+  //   else{
+  //     this.checked--;
+  //   }
+
+  //   console.log('Interest is '+ interest);
+  //   console.log('Checked is '+ this.checked);
+  // }
+
+
+  public onSelect(event: any, value: boolean) {
+    if( event.target.checked ) {
+      value = true;
       this.checked++;
-    }
-    else{
+      // make a call to a method to check if we should disable all unchecked checkboxs?
+    } else {
+      value = false;
       this.checked--;
     }
-
-    console.log('Interest is '+ interest);
-    console.log('Checked is '+ this.checked);
+    console.log('checked: ' + value);
+    console.log('Checked is ' + this.checked);
   }
 
-  /* onSubmit(){
-    //do something
-  } */
+  public isSelected(value: boolean): boolean {
+    if( value ){
+      console.log("isSelected was called and was true!")
+      return true;
+    } else {
+      console.log("isSelected was called and was false!")
+      return false;
+    }
+  }
 }
