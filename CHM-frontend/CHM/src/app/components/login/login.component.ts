610c501b36c8c9361f2bc2b022f4ca44c129040a
Chris Breniser <chrisbreniser@gmail.com>
2020-12-20 22:25:16 -0800

    further worked with tokens to allow validations and pulling out claims

--- CHM-frontend/CHM/src/app/components/login/login.component.ts
+++ CHM-frontend/CHM/src/app/components/login/login.component.ts
@@ -45,6 +45,9 @@ export class LoginComponent implements OnInit {
     this.loginService.login(this.username, this.password, this.rememberMe).subscribe(
       (resp) => {
 
+        console.log('resp.headers.get(\'ProfileId\') :' + resp.headers.get('ProfileId'));
+        this.loginService.setProfileId(resp.headers.get('ProfileId'));
+
         console.log('In response of submit method');
         this.errorMessage = `successful login`;
         console.log('resp.headers.get(this.authHeader): ' + resp.headers.get(this.authHeader));
